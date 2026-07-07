cafdc48d9f25d6958a159747234f6fa8e02598e5
ColdRiver <mailbox.rishit@gmail.com>
2026-07-07 13:39:23 +0530

    Update train.py

--- circular_economy_RL_lib/train.py
+++ circular_economy_RL_lib/train.py
@@ -1,61 +1,32 @@
-##########################################################################################
-# Machine Environment Config
-
-DEBUG_MODE = False
-USE_CUDA = not DEBUG_MODE
-CUDA_DEVICE_NUM = 0
-
-
-##########################################################################################
-# Path Config
-
+# Overwrite this file as: /kaggle/working/marl/circular_economy_RL_lib/train.py
 import os
 import sys
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, "..")  # for problem_def
-sys.path.insert(0, "../..")  # for utils
+# Prevent OpenMP and MKL thread-pool deadlocks
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
 
-
-##########################################################################################
-# import
-
-import logging
 import multiprocessing
 from utils import create_logger
-
-from trainer import Trainer
-
-
-##########################################################################################
-# parameters
+from trainer import BilevelTrainer  # Fixed: Imports BilevelTrainer instead of deprecated Trainer
 
 logger_params = {
     'log_file': {
-        'desc': 'train',
+        'desc': 'bilevel_train_marl',
         'filename': 'run_log'
     }
 }
 
-##########################################################################################
-# main
-
 def main():
     create_logger(**logger_params)
-    _print_config()
-    print(f'Number of CPUs: {multiprocessing.cpu_count()}')
-
-    trainer = Trainer()
-
+    print(f"System CPUs available: {multiprocessing.cpu_count()}")
+    print("Initializing Gaur et al. (2025) Bilevel Reinforcement Learning inside marl...")
+    
+    # Instantiate the hierarchical trainer
+    trainer = BilevelTrainer()  # Fixed: Instantiates BilevelTrainer
+    
+    # Launch training
     trainer.learn()
-
-def _print_config():
-    logger = logging.getLogger('root')
-    logger.info('DEBUG_MODE: {}'.format(DEBUG_MODE))
-    logger.info('USE_CUDA: {}, CUDA_DEVICE_NUM: {}'.format(USE_CUDA, CUDA_DEVICE_NUM))
-    [logger.info(g_key + "{}".format(globals()[g_key])) for g_key in globals().keys() if g_key.endswith('params')]
-
-##########################################################################################
 
 if __name__ == "__main__":
     main()
